b2fba8ca403dcabbc55a6dd7a8fb351b4199e2bf
Kiryll <orlovich090789@list.ru>
2023-12-25 21:44:24 +0300

    home_work

--- bookmobile.py
+++ bookmobile.py
@@ -43,7 +43,7 @@ def find_contact_by_last_name(): #2
         for i in file:
             i = i.split(', ')
             if i[0] == family:
-                print(i[1])         
+                print(f'Номер {family} - {i[1]}')         
 
 
 def find_contact_by_number():#5
@@ -73,10 +73,29 @@ def change_phone_number(): #3
         file.seek(0)
         file.writelines(content)
         file.truncate()
-        
-           
 
-           
+
+def delete_contact(): #4
+    data = input('Введите фамилию: ')
+    
+    with open('phonebook.txt', 'r') as file:
+        lines = file.readlines()
+    
+    with open('phonebook.txt', 'w') as file:
+        for line in lines:
+            key, value = line.strip().split(', ')
+            if key != data:
+                file.write(f'{key}, {value}\n')
+        return 'Номер удален' 
+
+def add_contact():
+    data_name = input('Введите фамилию: ') 
+    data_number = int(input('Введите номер: '))  
+    with open('phonebook.txt', 'a') as file:
+        file.write(f'{data_name}, {data_number}\n')
+        
+        return 'Абонент успешно добавлен!)'
+
 menu_choice()
 
 
